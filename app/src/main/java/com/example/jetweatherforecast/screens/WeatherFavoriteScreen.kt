570b9415482daac1c3623d25c8d1109f3047336d
aderemilekun <adelefatai@yahoo.com>
2023-03-08 12:06:40 +0100

    Finished setting up nav controller

---
+++ app/src/main/java/com/example/jetweatherforecast/screens/WeatherFavoriteScreen.kt
@@ -0,0 +1,9 @@
+package com.example.jetweatherforecast.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherFavoriteScreen(navController: NavController) {
+
+}
