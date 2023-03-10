d337f0d48a2096a86f8b5edad570318fc907ac88
aderemilekun <adelefatai@yahoo.com>
2023-03-10 13:37:16 +0100

    Humidity, Pressure, Wind widget

--- app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,8 +22,12 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
+import com.example.jetweatherforecast.R
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
+import com.example.jetweatherforecast.model.WeatherItem
+import com.example.jetweatherforecast.utils.formatDate
+import com.example.jetweatherforecast.utils.formatDecimals
 import com.example.jetweatherforecast.widget.WeatherAppBar
 
  @Composable
@@ -65,7 +70,8 @@ import com.example.jetweatherforecast.widget.WeatherAppBar
 
  @Composable
  fun MainContent(data: Weather) {
-     val imageUrl = "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"
+     val weatherItem = data.list[0]
+     val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png"
      Column(
          Modifier
              .padding(4.dp)
@@ -74,7 +80,7 @@ import com.example.jetweatherforecast.widget.WeatherAppBar
          horizontalAlignment = Alignment.CenterHorizontally
      ) {
          Text(
-             text = data.list[0].dt.toString(),
+             text = formatDate(weatherItem.dt), //Wed Nov 30
              style = MaterialTheme.typography.caption,
              color = MaterialTheme.colors.onSecondary,
              fontWeight = FontWeight.SemiBold,
@@ -91,20 +97,65 @@ import com.example.jetweatherforecast.widget.WeatherAppBar
              ) {
                  WeatherStateImage(imageUrl= imageUrl )
                  Text(
-                     text = "56",
+                     text = formatDecimals(weatherItem.temp.day) + "º",
                      style = MaterialTheme.typography.h4,
                      fontWeight = FontWeight.ExtraBold
                  )
                  Text(
-                     text = "Snow",
+                     text = weatherItem.weather[0].main,
                      fontStyle = FontStyle.Italic
                  )
-
-
              }
          }
+         HumidityWindPressureRow(weather= weatherItem)
      }
  }
+
+ @Composable
+ fun HumidityWindPressureRow(weather: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "Humidity icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(text = "${weather.humidity} %",
+                style = MaterialTheme.typography.caption)
+
+        }
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.pressure),
+                contentDescription = "Pressure icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(text = "${weather.pressure} psi",
+                style = MaterialTheme.typography.caption)
+
+        }
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "Wind icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(text = "${weather.humidity} mph",
+                style = MaterialTheme.typography.caption)
+
+        }
+    }
+
+ }
+
 
  @Composable
  fun WeatherStateImage(imageUrl: String) {
