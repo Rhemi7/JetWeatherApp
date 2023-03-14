a8a1bae0dde24513cf7f8600b4b20221a11defce
aderemilekun <adelefatai@yahoo.com>
2023-03-14 19:12:22 +0100

    Created favoriteViewModel

--- app/src/main/java/com/example/jetweatherforecast/repository/WeatherDbRepository.kt
+++ app/src/main/java/com/example/jetweatherforecast/repository/WeatherDbRepository.kt
@@ -1,7 +1,6 @@
 package com.example.jetweatherforecast.repository
 
 import com.example.jetweatherforecast.data.WeatherDao
-import com.example.jetweatherforecast.model.City
 import com.example.jetweatherforecast.model.Favorite
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -12,6 +11,7 @@ class WeatherDbRepository @Inject constructor(private val weatherDao: WeatherDao
 
     suspend fun insertFavorite(favorite: Favorite) = weatherDao.insertFavorite(favorite = favorite)
 
+    suspend fun updateFavorite(favorite: Favorite) = weatherDao.updateFavorite(favorite)
     suspend fun deleteAllFavorites() = weatherDao.deleteAllFavorites()
 
     suspend fun deleteFavorite(favorite: Favorite) = weatherDao.deleteFavorite(favorite = favorite)
