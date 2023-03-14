96fc5b6aba72d27478ab01e775d7e8741427955e
aderemilekun <adelefatai@yahoo.com>
2023-03-14 18:29:29 +0100

    Provided WeatherDao and WeatherDatabase in AppModule

--- app/src/main/java/com/example/jetweatherforecast/di/AppModule.kt
+++ app/src/main/java/com/example/jetweatherforecast/di/AppModule.kt
@@ -1,11 +1,17 @@
 package com.example.jetweatherforecast.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.jetweatherforecast.data.WeatherDao
+import com.example.jetweatherforecast.data.WeatherDatabase
 import com.example.jetweatherforecast.network.WeatherApi
 import com.example.jetweatherforecast.repository.WeatherRepository
 import com.example.jetweatherforecast.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -28,4 +34,19 @@ object AppModule {
             .build()
             .create(WeatherApi::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao =
+        weatherDatabase.weatherDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): WeatherDatabase =
+        Room.databaseBuilder(
+            context = context,
+            WeatherDatabase::class.java,
+            name = "weather_database"
+        ).fallbackToDestructiveMigration().build()
+
 }
