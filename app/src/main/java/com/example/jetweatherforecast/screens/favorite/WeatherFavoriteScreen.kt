2df4334ee4f1011b1e13e438df879b1ce9d387e0
aderemilekun <adelefatai@yahoo.com>
2023-03-15 21:32:59 +0100

    Displaying favorite cities on favorite screen

--- app/src/main/java/com/example/jetweatherforecast/screens/favorite/WeatherFavoriteScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/favorite/WeatherFavoriteScreen.kt
@@ -1,9 +1,92 @@
 package com.example.jetweatherforecast.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.model.Favorite
+import com.example.jetweatherforecast.screens.favorite.FavoriteViewModel
+import com.example.jetweatherforecast.widget.WeatherAppBar
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun WeatherFavoriteScreen(navController: NavController, favoriteViewModel: FavoriteViewModel = hiltViewModel()) {
+
+    Scaffold(topBar = { WeatherAppBar(
+        title = "Favorite Cities",
+        icon = Icons.Default.ArrowBack,
+        isMainScreen = false,
+        navController = navController
+    ) {navController.popBackStack()}}) {
+        Surface(modifier = Modifier
+            .padding(5.dp)
+            .fillMaxHeight()) {
+Column(
+    verticalArrangement = Arrangement.Top,
+    horizontalAlignment = Alignment.CenterHorizontally
+) {
+    val list = favoriteViewModel.favList.collectAsState().value
+
+    LazyColumn {
+        items(items = list) {
+            CityRow(it, navController = navController, favoriteViewModel)
+        }
+    }
+}
+        }
+
+    }
+}
 
 @Composable
-fun WeatherFavoriteScreen(navController: NavController) {
+fun CityRow(favorite: Favorite, navController: NavController, favoriteViewModel: FavoriteViewModel) {
+    Surface(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .height(50.dp)
+            .clickable { },
+        shape = CircleShape.copy(topEnd = CornerSize(6.dp)),
+        color = Color(0xffB2DFDB)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(text = favorite.city, modifier = Modifier.padding(4.dp))
+            Surface(
+                modifier = Modifier.padding(0.dp),
+                shape = CircleShape,
+                color = Color(0xffd1e3e1)
+            ) {
+                Text(
+                    text = favorite.country,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.caption
+                )
+            }
+
+            Icon(imageVector = Icons.Rounded.Delete, contentDescription = "Delete", modifier = Modifier.clickable {
+                favoriteViewModel.deleteFavorite(favorite)
+            }, tint = Color.Red.copy(alpha = 0.3f))
+        }
+    }
 
 }
